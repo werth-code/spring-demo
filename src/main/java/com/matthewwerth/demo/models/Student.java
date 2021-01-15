6db43f53acd5484f1c85a41868575d3d7e04c71e
Matt Werth <matthewwerth@gmail.com>
2021-01-15 08:49:53 -0500

    Added @Entity @Table To Student Class

--- src/main/java/com/matthewwerth/demo/models/Student.java
+++ src/main/java/com/matthewwerth/demo/models/Student.java
@@ -1,13 +1,94 @@
 package com.matthewwerth.demo.models;
 
-import com.matthewwerth.demo.person.Person;
-
+import javax.persistence.*;
 import java.time.LocalDate;
 
-public class Student extends Person {
+@Entity // An entity represents a table stored in a db. Every instance of an entity represents a row in the table.
+@Table // So, in our STUDENT TABLE we store ROWS of single STUDENT's
+public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
+        private Long id;
+        private String name;
+        private String email;
+        private LocalDate dob;
+        private Integer age;
 
-    public Student(long id, String name, String email, LocalDate dob, int age) {
-        super(id, name, email, dob, age);
+        public Student(Long id, String name, String email, LocalDate dob, Integer age) {
+            this.id = id;
+            this.name = name;
+            this.email = email;
+            this.dob = dob;
+            this.age = age;
+        }
+
+        public Student(String name, String email, LocalDate dob, Integer age) {
+            this.name = name;
+            this.email = email;
+            this.dob = dob;
+            this.age = age;
+        }
+
+        public Student() {
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public LocalDate getDob() {
+            return dob;
+        }
+
+        public void setDob(LocalDate dob) {
+            this.dob = dob;
+        }
+
+        public Integer getAge() {
+            return age;
+        }
+
+        public void setAge(Integer age) {
+            this.age = age;
+        }
+
+        @Override
+        public String toString() {
+            return "Student{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    ", email='" + email + '\'' +
+                    ", dob=" + dob +
+                    ", age=" + age +
+                    '}';
+        }
     }
-}
 
