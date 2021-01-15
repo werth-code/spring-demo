2d5efe63512a26e0d82e59612e80e8cceaef8e6b
Matt Werth <matthewwerth@gmail.com>
2021-01-15 07:29:48 -0500

    Added Student Class

--- src/main/java/com/matthewwerth/demo/DemoApplication.java
+++ src/main/java/com/matthewwerth/demo/DemoApplication.java
@@ -2,12 +2,22 @@ package com.matthewwerth.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
+@RestController 		// Allows this class to serve REST endpoints.
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping       // Allows us to GET this data from the URI specified. This function is auto called when we visit.
+	public List<String> hello() {
+		return List.of("Hello Matt!", "How Are You?");
 	}
 
 }
---
+++ src/main/java/com/matthewwerth/demo/models/Student.java
@@ -0,0 +1,70 @@
+package com.matthewwerth.demo.models;
+
+import java.time.LocalDateTime;
+
+public class Student {
+    private Long id;
+    private String name;
+    private String email;
+    private LocalDateTime dob;
+    private Integer age;
+
+    public Student(Long id, String name, String email, LocalDateTime dob, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.dob = dob;
+        this.age = age;
+    }
+
+    public Student(String name, String email, LocalDateTime dob, Integer age) {
+        this.name = name;
+        this.email = email;
+        this.dob = dob;
+        this.age = age;
+    }
+
+    public Student() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDateTime getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDateTime dob) {
+        this.dob = dob;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
+
